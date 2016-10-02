a4f40b9b5adf477af573e02c7269a10b444995c4
Alexander Batalschikov <abatalshikov@gmail.com>
2016-10-02 22:47:42 +0300

    Corrected name of firmware for HP 39gs

--- firmware/sys/target_50g/preamble.c
+++ firmware/sys/target_50g/preamble.c
@@ -6,7 +6,7 @@
  */
 
 char kinpo_preamble[16] __attribute__( (section (".preamble")))
-={'K','I','N','P','O','U','P','D','A','T','E','I','M','A','G','E'} ;
+={'K','I','N','P','O','H','P','3','9','G','+','I','M','A','G','E'} ;
 unsigned int filler[2] __attribute__( (section (".preamble"))) = { 0,0 };
 
 extern void startup(int);
